f27dee559191b819ba9ca74b9e226ad2af602010
Paula Leonova <paulaleonova@Paulas-MacBook-Pro.local>
2021-12-01 13:20:08 -0600

    Update inputs for summarizer

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
 
     with st.form(key='my_form'):
         text_input = st.text_area("Input any text you want to classify here:",ex_text)
-        minimum_tokens = 30
-        maximum_tokens = 100
+        # minimum_tokens = 30
+        # maximum_tokens = 100
         labels = st.text_input('Write any topic keywords you are interested in here (separate different topics with a ","):',ex_labels, max_chars=1000)
         labels = list(set([x.strip() for x in labels.strip().split(',') if len(x.strip()) > 0]))
         submit_button = st.form_submit_button(label='Submit')
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     if submit_button:
         if len(labels) == 0:
             st.write('Enter some text and at least one possible topic to see predictions.')
-        summary = summarizer_gen(summarizer, sequence=text_input, max_length=maximum_tokens, min_length=minimum_tokens, do_sample=False)
+        summary = summarizer_gen(summarizer, sequence=text_input, maximum_tokens = 30, minimum_tokens = 100, do_sample=False)
         st.text(summary)
         top_topics, scores = classifier_zero(classifier, sequence=text_input, labels=labels, multi_class=True)
         plot_result(top_topics[::-1][-10:], scores[::-1][-10:])
