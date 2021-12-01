58b7499a080eb40cad555d38f8bddef7b2636970
Paula Leonova <paulaleonova@Paulas-MacBook-Pro.local>
2021-12-01 14:21:41 -0600

    Add and update sent_tokenize

--- hf_model.py
+++ hf_model.py
@@ -4,28 +4,27 @@ import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 import nltk
-
 nlp = nltk.download('punkt')
 
-# # Reference: https://discuss.huggingface.co/t/summarization-on-long-documents/920/7
-# def create_nest_sentences(document, token_max_length = 1024):
-#   nested = []
-#   sent = []
-#   length = 0
-#   for sentence in nltk.sent_tokenize(document):
-#     tokens_in_sentence = tokenizer(sentence, truncation=False, padding=False)[0] # hugging face transformer tokenizer
-#     length += len(tokens_in_sentence)
+# Reference: https://discuss.huggingface.co/t/summarization-on-long-documents/920/7
+def create_nest_sentences(document, token_max_length = 1024):
+  nested = []
+  sent = []
+  length = 0
+  for sentence in nlp.sent_tokenize(document):
+    tokens_in_sentence = tokenizer(sentence, truncation=False, padding=False)[0] # hugging face transformer tokenizer
+    length += len(tokens_in_sentence)
 
-#     if length < token_max_length:
-#       sent.append(sentence)
-#     else:
-#       nested.append(sent)
-#       sent = []
-#       length = 0
+    if length < token_max_length:
+      sent.append(sentence)
+    else:
+      nested.append(sent)
+      sent = []
+      length = 0
 
-#   if sent:
-#     nested.append(sent)
-#   return nested
+  if sent:
+    nested.append(sent)
+  return nested
 
 # Reference: https://huggingface.co/facebook/bart-large-cnn
 def load_summary_model():
