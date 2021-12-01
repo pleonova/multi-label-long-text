afeba32bd04f9644e6a7186a03298667b2f99609
Paula Leonova <paulaleonova@Paulas-MacBook-Pro.local>
2021-12-01 09:59:58 -0600

    Download punkt model from nltk

--- hf_model.py
+++ hf_model.py
@@ -1,5 +1,10 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification,pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import torch
+import nltk
+
+nltk.download('punkt')
+
+
 
 def load_model():
     model_name = "facebook/bart-large-mnli"
@@ -8,7 +13,7 @@ def load_model():
     classifier = pipeline(task='zero-shot-classification', model=model, tokenizer=tokenizer, framework='pt')
     return classifier
 
-def classifier_zero(classifier,sequence:str,labels:list,multi_class:bool):
-    outputs=classifier(sequence, labels,multi_label=multi_class)
+def classifier_zero(classifier, sequence:str, labels:list, multi_class:bool):
+    outputs=classifier(sequence, labels, multi_label=multi_class)
     return outputs['labels'], outputs['scores']
 
